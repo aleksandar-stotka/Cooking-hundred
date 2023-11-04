343f4af85607deb9216ee95a8021c66dd820b2ed
Aleksandar Angelevski <aangelevski6@gmail.com>
2023-11-03 19:12:24 -0700

    fetchData

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css'
+import Products from './components/Products';
+import { useEffect } from 'react';
+import { useFetch } from './hooks/useFetch (1)';
 
 function App() {
-  return (
+   const {data} = useFetch("https://fakestoreapi.com/products")
+   
+    console.log(data)
+   
+      
+   return (
     <div className="App">
       
+       {data && data.map(item => {
+        return  <Products key={item.id} {...item}/>
+       })}
     </div>
   );
 }
---
+++ src/components/Products.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useFetch } from '../hooks/useFetch (1)';
+const Products = ({title}) => {
+  
+
+    return (
+        <div>
+       <p>{title}</p>
+        </div>
+    );
+}
+
+export default Products;
